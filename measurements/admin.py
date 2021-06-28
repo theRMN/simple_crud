876b8e57b044b7ffe97f6f41c5dc4d7d7510bf21
theRMN <roman.vassev@gmail.com>
2021-06-28 20:31:08 +0600

    project deployed

---
+++ measurements/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Project, Measurement
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    ...
---
+++ measurements/views.py
@@ -0,0 +1,12 @@
+from rest_framework.viewsets import ModelViewSet
+from django.http import HttpResponse, JsonResponse
+
+
+class ProjectViewSet(ModelViewSet):
+    """ViewSet для проекта."""
+    # TODO: добавьте конфигурацию для объекта
+
+
+class MeasurementViewSet(ModelViewSet):
+    """ViewSet для измерения."""
+    # TODO: добавьте конфигурацию для измерения
