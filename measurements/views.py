fcf81251db178fc5c8d7feba53d68f1e20779c42
theRMN <roman.vassev@gmail.com>
2021-06-29 19:53:24 +0600

    relization 4

--- measurements/views.py
+++ measurements/views.py
@@ -1,8 +1,6 @@
 from .models import Project, Measurement
 from .serializers import ProjectSerializer, MeasurementSerializer
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
 
 # class ProjectViewSet(ModelViewSet):
 #     """ViewSet для проекта."""
@@ -14,15 +12,6 @@ from rest_framework.views import APIView
 #     # TODO: добавьте конфигурацию для измерения
 
 
-class ProjectApiView(APIView):
-    def get(self, request, *args, **kwargs):
-        objects = Project.objects.all()
-        serializer = ProjectSerializer(objects, many=True)
-        return Response(serializer.data, status=HTTP_200_OK)
-
-    def post(self, request, *args, **kwargs):
-        serializer = ProjectSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        obj = Project.objects.create(**serializer.validated_data)
-        context = ProjectSerializer(obj)
-        return Response(context.data, status=HTTP_201_CREATED)
+class ProjectApiView(ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
