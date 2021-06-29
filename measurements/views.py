ecffc6cd4f5627a2696904c8bc82fb4f323da23b
theRMN <roman.vassev@gmail.com>
2021-06-29 20:16:14 +0600

    final relization

--- measurements/views.py
+++ measurements/views.py
@@ -1,17 +1,13 @@
 from .models import Project, Measurement
 from .serializers import ProjectSerializer, MeasurementSerializer
-from rest_framework.generics import ListCreateAPIView
-
-# class ProjectViewSet(ModelViewSet):
-#     """ViewSet для проекта."""
-#     # TODO: добавьте конфигурацию для объекта
-#
-#
-# class MeasurementViewSet(ModelViewSet):
-#     """ViewSet для измерения."""
-#     # TODO: добавьте конфигурацию для измерения
+from rest_framework.viewsets import ModelViewSet
 
 
-class ProjectApiView(ListCreateAPIView):
+class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+
+class MeasurementViewSet(ModelViewSet):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
