1d391c72a4273a79e132613985cfeab64cc513d9
theRMN <roman.vassev@gmail.com>
2021-06-30 14:05:11 +0600

    optional task

--- measurements/models.py
+++ measurements/models.py
@@ -1,36 +1,40 @@
 from django.db import models
 
 
-class Project(models.Model):
-    """Объект на котором проводят измерения."""
-
-    name = models.TextField()
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+class TimestampFields(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True
     )
     updated_at = models.DateTimeField(
         auto_now=True
     )
+
+    class Meta:
+        abstract = True
+
+
+class Project(TimestampFields):
+    """Объект на котором проводят измерения."""
+
+    name = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
 
     class Meta:
         verbose_name = 'Объект'
         verbose_name_plural = 'Объекты'
 
 
-class Measurement(models.Model):
+class Measurement(TimestampFields):
     """Измерение температуры на объекте."""
 
     value = models.FloatField()
+    photo = models.ImageField(max_length=None, null=True, blank=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
 
     class Meta:
         verbose_name = 'Измерение температуры'
         verbose_name_plural = 'Измерения температуры'
+
+
+
